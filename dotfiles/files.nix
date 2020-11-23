98b0a1b7a525cbc480ca58221d6b3719bb7e7502
Ja0nz <mail@ja.nz>
2020-11-23 12:58:29 +0100

    Moved emacs config out of home-manager. Too many timely rebuilds

--- dotfiles/files.nix
+++ dotfiles/files.nix
@@ -14,11 +14,11 @@ in
   home.file.".authinfo.gpg".source = "${dots}/protected/authinfo.gpg";
 #### Emacs - needs some cleanup #####
 #  home.file.".emacs.d/prelude-pinned-packages.el".source = "${dots}/pkgs/emacs/prelude-pinned-packages.el";
-  home.file.".emacs.d/personal/custom-keybindings.el".source = "${dots}/pkgs/emacs/personal/custom-keybindings.el";
-  home.file.".emacs.d/personal/mail.el".source = "${dots}/pkgs/emacs/personal/mail.el";
-  home.file.".emacs.d/personal/misc.el".source = "${dots}/pkgs/emacs/personal/misc.el";
-  home.file.".emacs.d/personal/nim.el".source = "${dots}/pkgs/emacs/personal/nim.el";
-  home.file.".emacs.d/personal/org-mode.el".source = "${dots}/pkgs/emacs/personal/org-mode.el";
-  home.file.".emacs.d/personal/prelude-modules.el".source = "${dots}/pkgs/emacs/personal/prelude-modules.el";
-  home.file.".emacs.d/personal/setup-ligatures.el".source = "${dots}/pkgs/emacs/personal/setup-ligatures.el";
+#  home.file.".emacs.d/personal/custom-keybindings.el".source = "${dots}/pkgs/emacs/personal/custom-keybindings.el";
+#  home.file.".emacs.d/personal/mail.el".source = "${dots}/pkgs/emacs/personal/mail.el";
+#  home.file.".emacs.d/personal/misc.el".source = "${dots}/pkgs/emacs/personal/misc.el";
+#  home.file.".emacs.d/personal/nim.el".source = "${dots}/pkgs/emacs/personal/nim.el";
+#  home.file.".emacs.d/personal/org-mode.el".source = "${dots}/pkgs/emacs/personal/org-mode.el";
+#  home.file.".emacs.d/personal/prelude-modules.el".source = "${dots}/pkgs/emacs/personal/prelude-modules.el";
+#  home.file.".emacs.d/personal/setup-ligatures.el".source = "${dots}/pkgs/emacs/personal/setup-ligatures.el";
 }
