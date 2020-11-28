020971b1479db0a6b3a48f8484e0679e3d15c8de
Ja0nz <mail@ja.nz>
2020-11-28 14:05:12 +0100

    Enabled racket mode

---
+++ dotfiles/pkgs/emacs/personal/racket.el
@@ -0,0 +1,16 @@
+;;; racket.el --- Racket language mode -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Just switch on racket-mode
+
+;;; Code:
+
+(require 'use-package)
+
+(use-package racket-mode
+  :ensure t)
+
+(provide 'racket-mode)
+
+;;; racket.el ends here
