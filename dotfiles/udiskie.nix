34abfa1030a539fedf2d0dfc6f10d5e7a70913fe
Ja0nz <mail@ja.nz>
2020-12-08 19:39:02 +0100

    Refactored killswitch

--- dotfiles/udiskie.nix
+++ dotfiles/udiskie.nix
@@ -1,7 +1,16 @@
-{ ... }:
+{ config, ... }:
 
+let
+  inherit (config) dots;
+in
 {
   services.udiskie = {
     enable = true;
   };
+  home.file.".config/udiskie/config.yml".text = ''
+    device_config:
+      - id_uuid: b3ec2286-cc94-4540-b5bc-3446fabb2242
+      - keyfile: ./killswitch_random_keyfile
+  '';
+  home.file.".config/udiskie/killswitch_random_keyfile".source = "${dots}/protected/killswitch_random_keyfile";
 }
