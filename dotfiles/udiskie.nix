31cba38bdbc6d6d848e30c863219bd191f405e65
Ja0nz <mail@ja.nz>
2020-12-09 10:38:43 +0100

    Fixed path for killswitch file

--- dotfiles/udiskie.nix
+++ dotfiles/udiskie.nix
@@ -10,7 +10,7 @@ in
   home.file.".config/udiskie/config.yml".text = ''
     device_config:
       - id_uuid: b3ec2286-cc94-4540-b5bc-3446fabb2242
-      - keyfile: ./killswitch_random_keyfile
+      - keyfile: $HOME/.config/udiskie/killswitch_random_keyfile
   '';
   home.file.".config/udiskie/killswitch_random_keyfile".source = "${dots}/protected/killswitch_random_keyfile";
 }
