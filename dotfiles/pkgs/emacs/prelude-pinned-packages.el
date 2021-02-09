7fc66eeb17041817cdca56c0c7904c452ec5b7b4
Ja0nz <mail@ja.nz>
2021-02-09 09:40:17 +0000

    Added org upstream repo

--- dotfiles/pkgs/emacs/prelude-pinned-packages.el
+++ dotfiles/pkgs/emacs/prelude-pinned-packages.el
@@ -1,7 +1,5 @@
-(add-to-list 'package-archives
-             '("gnu-copy" . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/gnu/") t)
+;; (add-to-list 'package-archives '("gnu-copy" . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/gnu/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 
-(setq package-pinned-packages
-      '(
-        (undo-tree . "gnu-copy")
-        ))
+;; (setq package-pinned-packages
+;;       '((undo-tree . "gnu-copy")))
