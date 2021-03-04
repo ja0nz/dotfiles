46058e411b9f2c76fc46bd7b4032cdb2f06a41df
Ja0nz <mail@ja.nz>
2021-03-04 12:01:25 +0000

    [:Emacs:] New fish theme

--- dotfiles/fish_prompt.nix
+++ dotfiles/fish_prompt.nix
@@ -1,8 +1,14 @@
 { fetchFromGitHub }:
 
+# fetchFromGitHub {
+#   owner = "oh-my-fish";
+#   repo = "theme-agnoster";
+#   rev = "3a43a778676251c940fa11b21dbd6d311197624c";
+#   sha256 = "1qc6srdg8ar9k7p97yg2q0naqdd260wxkljf0r91gh2hidw583xa";
+# }
 fetchFromGitHub {
-  owner = "oh-my-fish";
-  repo = "theme-agnoster";
-  rev = "3a43a778676251c940fa11b21dbd6d311197624c";
-  sha256 = "1qc6srdg8ar9k7p97yg2q0naqdd260wxkljf0r91gh2hidw583xa";
+  owner = "wyqydsyq";
+  repo = "emoji-powerline";
+  rev = "eab03a3973c3c121433f5f3c588f833c34441fd5";
+  sha256 = "0brz45wzmyfpl59dfvkh9ck216354lw5xlm96rdp2a10kv03ngqj";
 }
